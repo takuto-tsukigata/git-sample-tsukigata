f8e429cbff0ccfccdf4ca27bb25384dba0da8e24
tsukigata-takuto <takuto.tsukigata@rakus-partners.co.jp>
2022-02-15 19:21:27 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleTsukigataApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleTsukigataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleTsukigataApplication.class, args);
+	}
+
+}
